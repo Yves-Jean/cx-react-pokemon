506f566517bc289aa0155b43dd6701d0d32bf610
Yves kouayip <kamwayves37@gmail.com>
2020-12-04 20:39:20 +0100

    defined helper class

--- client/src/components/toolbar/Toolbar.jsx
+++ client/src/components/toolbar/Toolbar.jsx
@@ -4,7 +4,7 @@ import "./toolbar.scss";
 const Toolbar = () => {
   return (
     <header className="header container">
-      <nav className="nav-bar display-flex flex-justify-end">
+      <nav className="nav-bar display-flex flex-justify-end pd-y-2">
         <button className="btn btn-pink">light theme</button>
       </nav>
     </header>
