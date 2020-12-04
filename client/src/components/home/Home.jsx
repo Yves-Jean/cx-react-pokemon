8159f57dac929ddfe764a945a91531344cb1a19d
Yves kouayip <kamwayves37@gmail.com>
2020-12-04 21:35:52 +0100

    set logo to home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "./home.scss";
+
+import logo from '../../assets/images/logo.svg'; //! Change logo to logo pokedex
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className="container">
+      <NavLink to="/">
+      <img src={logo} alt="logo"/>
+      </NavLink>
+    </main>
   );
 };
 
