b7f210be704942e7703afc390d4968e26fc97538
Yves kouayip <kamwayves37@gmail.com>
2020-12-04 21:44:17 +0100

    set section Search Bar and section List Pokemon

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./home.scss";
-
-import logo from '../../assets/images/logo.svg'; //! Change logo to logo pokedex
+import logo from "../../assets/images/logo.svg"; //! Change logo to logo pokedex
 import { NavLink } from "react-router-dom";
+import SearchBar from '../searchBar/SearchBar';
+import PokemonList from '../pokemonList/PokemonList';
 
 const Home = () => {
   return (
     <main className="container">
       <NavLink to="/">
-      <img src={logo} alt="logo"/>
+        <img src={logo} alt="logo" />
       </NavLink>
+      <SearchBar/>
+      <PokemonList/>
     </main>
   );
 };
--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SearchBar = () => {
-  return <div></div>;
+  return <div>Seach Pokemon</div>;
 };
 
 export default SearchBar;
