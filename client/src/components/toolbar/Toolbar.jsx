cc5942426b463988f413cf83d272042e03c7e16c
Yves kouayip <kamwayves37@gmail.com>
2020-12-05 19:32:10 +0100

    consume context therme to component toolbar to change a theme

--- client/src/components/toolbar/Toolbar.jsx
+++ client/src/components/toolbar/Toolbar.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import { ThemeContext } from "../../contexts/ThemeContext";
+import React, { useContext } from "react";
 import "./toolbar.scss";
 
 const Toolbar = () => {
+  const [theme, setTheme] = useContext(ThemeContext);
+  const getSwithTheme = () => (theme === "light" ? "dark" : "light");
+
   return (
     <header className="header container">
       <nav className="nav-bar display-flex flex-justify-end pd-y-2">
-        <button className="btn btn-pink">light theme</button>
+        <button
+          className="btn btn-pink"
+          onClick={() => {
+            setTheme(getSwithTheme());
+          }}
+        >
+          {getSwithTheme()} theme
+        </button>
       </nav>
     </header>
   );
