009ccb1efd45620a34282e12420d4e9335740d3f
Yves kouayip <kamwayves37@gmail.com>
2020-12-05 12:55:30 +0100

    update size width input search bar

--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./searchBar.scss";
 
 const SearchBar = () => {
-  return <div>Seach Pokemon</div>;
+  return (
+    <div className="search">
+      <input placeholder="Search Pokemon" type="text" name="search" />
+      <button>btn</button>
+    </div>
+  );
 };
 
 export default SearchBar;
