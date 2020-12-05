cf8892ac9eede8128c10f99ad22e0c626aed4d60
Yves kouayip <kamwayves37@gmail.com>
2020-12-05 18:44:11 +0100

    refactoring api method get pokemon

--- server/PokemonsProvider.js
+++ server/PokemonsProvider.js
@@ -8,8 +8,19 @@ const PokemonsProvider = {
       .groupBy("pokemon.numéro", "pokemon.nom")
       .orderBy("pokemon.numéro", "asc");
   },
+
   getAllPokemons() {
     return knex("pokemon").select(["numéro", "nom", "numero"]);
+  },
+
+  getPokemon(id) {
+    return knex("pokemon")
+      .innerJoin("attaques", "pokemon.numéro", "attaques.pokemon_id")
+      .select(["pokemon.*", knex.raw("json_agg(attaques) as attaques")])
+      .where("numéro", id)
+      .orWhere("numero", id)
+      .groupBy("pokemon.numéro", "pokemon.nom")
+      .orderBy("pokemon.numéro", "asc");
   },
   getAttaques(id) {
     return knex.select().from("attaques").where("pokemon_id", id);
