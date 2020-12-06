1e8533ae7cbf7b03f58f4214f260c93e0945d16e
Yves kouayip <kamwayves37@gmail.com>
2020-12-06 18:43:45 +0100

    update style component identity

--- client/src/components/pokemonDetails/pokemonIdentity/PokemonIdentity.jsx
+++ client/src/components/pokemonDetails/pokemonIdentity/PokemonIdentity.jsx
@@ -5,12 +5,14 @@ const PokemonIdentity = ({ pokemonIndentity }) => {
   return (
     <div className="container">
       <h2>Identity</h2>
-      <div className="identity">
+      <div className="identity ">
         {Object.keys(pokemonIndentity).map((key, i) => {
           if (pokemonIndentity[key]) {
             return (
               <div className="identity-item" key={i}>
-                <div className="key">{key}</div>
+                <div className="key">
+                  {key.charAt(0).toUpperCase() + key.slice(1)}
+                </div>
                 <div className="value">{pokemonIndentity[key]}</div>
               </div>
             );
